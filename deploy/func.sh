315fc3fa7e967bce285a37ca33abd5093aa04c07
meimei <meimei@huawei.com>
2015-06-01 12:22:55 +0800

    modify func.sh to add sudo

--- deploy/func.sh
+++ deploy/func.sh
@@ -1,26 +1,26 @@
 function tear_down_machines() {
-    virtmachines=$(virsh list --name |grep pxe)
+    virtmachines=$(sudo virsh list --name |grep pxe)
     for virtmachine in $virtmachines; do
         echo "destroy $virtmachine"
-        virsh destroy $virtmachine
+        sudo virsh destroy $virtmachine
         if [[ "$?" != "0" ]]; then
             echo "destroy instance $virtmachine failed"
             exit 1
         fi
     done
-    virtmachines=$(virsh list --all --name |grep pxe)
+    virtmachines=$(sudo virsh list --all --name |grep pxe)
     for virtmachine in $virtmachines; do
         echo "undefine $virtmachine"
-        virsh undefine $virtmachine
+        sudo virsh undefine $virtmachine
         if [[ "$?" != "0" ]]; then
             echo "undefine instance $virtmachine failed"
             exit 1
         fi
     done
-    vol_names=$(virsh vol-list default |grep .img | awk '{print $1}')
+    vol_names=$(sudo virsh vol-list default |grep .img | awk '{print $1}')
     for vol_name in $vol_names; do 
     echo "vol-list $vol_name"
-        virsh vol-delete  $vol_name  --pool default
+        sudo virsh vol-delete  $vol_name  --pool default
         if [[ "$?" != "0" ]]; then
             echo "undefine instance $virtmachine failed"
             exit 1
