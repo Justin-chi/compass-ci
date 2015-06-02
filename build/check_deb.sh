142a14dd7c7fa48b86756d9e2be36a17b6813200
Justin-chi <chigang@huawei.com>
2015-06-02 04:07:36 +0000

    add check_deb.sh

---
+++ build/check_deb.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+set -e
+
+export DEB_DIR="$1"
+export MD5SUMFILE="${DEB_DIR}/md5deb.txt"
+export MD5SUMFILE_BAK="${DEB_DIR}/md5deb.txt.bk"
+OLDSUM=
+NEWSUM=
+
+if [ ! -f ${MD5SUMFILE_BAK} ]
+then
+	echo "MD5 file is not exist, so need rebuild iso"
+	find ${DEB_DIR} -type f -print0 | xargs -0 md5sum | grep -v "md5deb.txt" | tee ${MD5SUMFILE}
+	mv ${MD5SUMFILE} ${MD5SUMFILE_BAK}
+	exit 1
+else
+	echo "MD5 file is exist, so check whether update"
+	find ${DEB_DIR} -type f -print0 | xargs -0 md5sum | grep -v "md5deb.txt" | tee ${MD5SUMFILE}
+	NEWSUM=`md5sum ${MD5SUMFILE} |cut -d' ' -f1`
+	OLDSUM=`md5sum ${MD5SUMFILE_BAK} |cut -d' ' -f1`
+	if [ ${NEWSUM} != ${OLDSUM} ]
+	then
+		echo "md5sum is different, so need rebuild iso"
+		mv ${MD5SUMFILE} ${MD5SUMFILE_BAK}
+		exit 1
+	else
+		echo "md5sum is the same, no need rebuild"
+		exit 0
+	fi
+fi
+
