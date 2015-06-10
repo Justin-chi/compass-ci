3b30ca09ccf6815eafb5de6e85b7c62095dbb6c1
baiguoku <baiguoku@huawei.com>
2015-06-10 10:11:00 +0800

    add ci notify for ansible excute result

--- deploy/deploy-vm.sh
+++ deploy/deploy-vm.sh
@@ -16,6 +16,9 @@ fi
 cp bin/switch_virtualenv.py.template bin/switch_virtualenv.py
 sed -i "s|\$PythonHome|$VIRTUAL_ENV|g" bin/switch_virtualenv.py
 #source ../compass-install/ci/allinone.conf
+/usr/bin/expect ${SCRIPT_DIR}/../deploy/remote_excute.exp \
+    "scp -r ${SCRIPT_DIR}/../deploy/status_callback.py root@${COMPASS_SERVER}:/opt/compass/bin/ansible_callbacks" \
+    vagrant
 bin/client.py --logfile= --loglevel=debug --logdir= --compass_server="${COMPASS_SERVER_URL}" \
 --compass_user_email="${COMPASS_USER_EMAIL}" --compass_user_password="${COMPASS_USER_PASSWORD}" \
 --cluster_name="${CLUSTER_NAME}" --language="${LANGUAGE}" --timezone="${TIMEZONE}" \
@@ -38,6 +41,8 @@ deploy_result=$?
 #tear_down_machines
 #cd ../compass-install
 #sudo vagrant destroy compass_nodocker
+/usr/bin/expect ${SCRIPT_DIR}/../deploy/remote_excute.exp \
+    "ssh root@${COMPASS_SERVER} rm -rf /opt/compass/bin/ansible_callbacks/status_callback.py" vagrant
 sudo rm -rf compass-core
 sudo rm -rf compass-install
 if [[ $deploy_result != 0 ]]; then
